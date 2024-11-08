2128e83b533705eb2e51dac69cf3101e8a4bf54e
Belu Cacciamani <cacciamanibelen@gmail.com>
2024-11-08 01:54:12 -0300

    Agrego estilos y  funcionalidad de botones eliminar y modificar productos

--- InventoryTech/js/main.js
+++ InventoryTech/js/main.js
@@ -28,6 +28,14 @@ const producto5 = new Producto("samsung galaxy", 5400000, 20);
 
 let lista = [producto1, producto2, producto3, producto4, producto5];
 
+//PARA TOMAR LO AGREGADO EN LINEA Y GUARDARLO EN LOCAL STORAGE
+
+if (localStorage.getItem("productos")) {
+  lista = JSON.parse(localStorage.getItem("productos"));
+} else {
+  lista = lista;
+}
+
 const listaProductos = document.querySelector("#productos");
 
 function filtrarProductos() {
@@ -39,7 +47,7 @@ function filtrarProductos() {
     showConfirmButton: true,
 
     preConfirm: (palabraClave) => {
-      palabraClave = palabraClave.trim().toUpperCase(); // .trim saco espacios  y convierto en mays
+      palabraClave = palabraClave.trim().toUpperCase(); // .trim saco espacios  y convierto en mayúscula
       let resultado = lista.filter((producto) =>
         producto.nombre.toUpperCase().includes(palabraClave)
       );
@@ -123,10 +131,161 @@ function agregarProductos() {
   });
 }
 
+function agregarProductos() {
+  Swal.fire({
+    title: "Agregar Producto",
+    html: `<label>Nombre:</label> <input id="nombre-input" class="swal2-input" type="text" autofocus>
+
+           <label>Precio:</label><input id="precio-input" class="swal2-input" type="number" step="0.01">
+
+           <label>Stock:</label><input id="stock-input" class="swal2-input" type="number" step="1">`,
+    showCancelButton: true,
+    confirmButtonText: "Agregar",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      let nombre = document.getElementById("nombre-input").value.trim();
+      let precio = parseFloat(
+        document.getElementById("precio-input").value.trim()
+      );
+      let stock = parseInt(document.getElementById("stock-input").value);
+
+      if (isNaN(precio) || isNaN(stock) || nombre == "") {
+        Swal.fire({
+          title: "error",
+          icon: "error",
+          text: "por favor ingrese datos validos",
+        });
+        return;
+      }
+
+      let producto = new Producto(nombre, precio, stock);
+      if (lista.some((elemento) => elemento.nombre === producto.nombre)) {
+        Swal.fire({
+          title: "error",
+          icon: "error",
+          text: "Ya existe el producto en la lista",
+        });
+        return;
+      }
+
+      lista.push(producto);
+
+      Swal.fire({
+        icon: "success",
+        title: "Producto agregado",
+        text: `Se agrego el producto ${producto.nombre} a la lista`,
+      });
+
+      console.table(lista);
+    }
+  });
+}
+
+function eliminarProducto() {
+  Swal.fire({
+    title: "Eliminar producto",
+    input: "select",
+    inputOptions: lista.reduce((options, producto, index) => {
+      options[index] = producto.nombre;
+      return options;
+    }),
+
+    inputPlaceholder: "Seleccione un producto",
+    showCancelButton: true,
+    confirmButtonText: "Eliminar",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const productoIndex = result.value;
+      const productoEliminado = lista.splice(productoIndex, 1);
+      localStorage.setItem("productos", JSON.stringify(lista));
+
+      Swal.fire({
+        icon: "success",
+        title: "Producto eliminado",
+        text: `Se a eliminado el producto ${productoEliminado[0].nombre}`,
+      });
+    }
+  });
+}
+
+function modificarProducto() {
+  Swal.fire({
+    title: "Modificar producto existente",
+    input: "select",
+    inputOptions: lista.reduce((options, producto, index) => {
+      options[index] = producto.nombre;
+      return options;
+    }),
+
+    inputPlaceholder: "Seleccione producto a modificar",
+    showCancelButton: true,
+    confirmButtonText: "Modificar",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const indiceProducto = result.value; // Índice del producto seleccionado
+
+      // Obtener los datos actuales del producto para mostrarlos al usuario
+      const productoActual = lista[indiceProducto];
+      Swal.fire({
+        title: "Modificar Producto",
+        html: `<label>Nombre (actual:${productoActual.nombre})</label> <input id="nombre-input" class="swal2-input" type="text" value="${productoActual.nombre}" autofocus>
+
+           <label>Precio (actual: ${productoActual.precio})</label><input id="precio-input" class="swal2-input" type="number" value="${productoActual.precio}" step="0.1">
+
+           <label>Stock (actual: ${productoActual.stock})</label><input id="stock-input" class="swal2-input" type="number" value="${productoActual.stock}" step="1">`,
+        showCancelButton: true,
+        confirmButtonText: "Guardar",
+        cancelButtonText: "Cancelar",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const nombre = document.getElementById("nombre-input").value.trim();
+          const precio = parseFloat(
+            document.getElementById("precio-input").value.trim()
+          );
+          const stock = parseInt(document.getElementById("stock-input").value);
+
+          if (isNaN(precio) || isNaN(stock) || nombre == "") {
+            Swal.fire({
+              title: "error",
+              icon: "error",
+              text: "por favor ingrese datos validos",
+            });
+            return;
+          }
+
+          lista[indiceProducto].nombre = nombre;
+          lista[indiceProducto].precio = precio;
+          lista[indiceProducto].stock = stock;
+
+          Swal.fire({
+            icon: "success",
+            title: "Producto modificado",
+            text: `Se modifico exitosamente el producto ${lista[indiceProducto].nombre} a la lista`,
+          });
+
+          console.table(lista);
+        }
+      });
+    } else {
+    }
+  });
+}
+
 // BOTONERA
+
+const mostrarProductos = document.querySelector("#productos");
 
 const filtrarBtn = document.getElementById("filtrar");
 filtrarBtn.addEventListener("click", filtrarProductos);
 
 const agregarBtn = document.getElementById("agregar");
 agregarBtn.addEventListener("click", agregarProductos);
+
+const eliminarBtn = document.getElementById("eliminar");
+eliminarBtn.addEventListener("click", eliminarProducto);
+
+const editarBtn = document.getElementById("modificar");
+editarBtn.addEventListener("click", modificarProducto);
